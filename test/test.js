0a37a5a2636b8619992a2f460bd2135ef42dc5d1
jjf001 <xingzhewujiang1999@163.com>
2015-07-22 19:16:03 +0800

    update test case

--- test/test.js
+++ test/test.js
@@ -18,76 +18,86 @@ describe('Swiper', function () {
     });
 
 
-    describe('swiper should have the following methods:', function(){
-        var methods = [
-            '_init',
-            '_bind',
-            '_getItems',
-            '_show',
-            '_addClass',
-            '_removeClass',
-            'next',
-            'on'
-        ];
-        var i = 0;
-        var len = methods.length;
-        for (i; i < len; i += 1) {
-            (function (i) {
-                it('.' + methods[i] + '()', function (done) {
-                    assert(typeof swiper[methods[i]] === 'function');
-                    done()
-                });
-            }(i));
-        }
 
-    });
+});
 
-    describe('swiper should have the following properties', function(){
-        var properties = ['version', '_default', '_options', '_start', '_move', '_end', '_prev', '_current', '_offset', '_eventHandlers', '_cache',
+describe('swiper should have the following methods:', function(){
+    var methods = [
+        '_init',
+        '_bind',
+        '_getItems',
+        '_show',
+        '_addClass',
+        '_removeClass',
+        'next',
+        'on'
+    ];
+    var i = 0;
+    var len = methods.length;
+    for (i; i < len; i += 1) {
+        (function (i) {
+            it('.' + methods[i] + '()', function (done) {
+                assert(typeof swiper[methods[i]] === 'function');
+                done()
+            });
+        }(i));
+    }
+
+});
+
+describe('swiper should have the following properties', function(){
+    var properties = ['version', '_default', '_options', '_start', '_move', '_end', '_prev', '_current', '_offset', '_eventHandlers', '_cache',
         '$container', '$items', 'count', '_width', '_height'];
-        for (var i = 0, len = properties.length; i < len; i++){
+    for (var i = 0, len = properties.length; i < len; i++){
+        (function(i){
+            it('.' + properties[i] + '', function(done){
+                assert(swiper[properties[i]] !== undefined);
+                done();
+            });
+        })(i);
+    }
+});
+
+describe('it should add classes to dom', function(){
+    var $slider = swiper.$items[swiper._current];
+    var $items = $slider.querySelectorAll('*[toggle-class]');
+    Array.prototype.forEach.call($items, function($item, index){
+        var clazz = $item.getAttribute('toggle-class').split(/\s+/);
+        for(var i = 0, len = clazz.length; i < len; i++){
             (function(i){
-                it('.' + properties[i] + '', function(done){
-                    assert(swiper[properties[i]] !== undefined);
-                    done();
+                it('.' + clazz, function(done){
+                    setTimeout(function(){
+                        assert($item.className.indexOf(clazz[i]) !== -1);
+                        done();
+                    }, 1000);
                 });
             })(i);
         }
     });
+});
 
-    describe('it should add classes to dom', function(){
-        var $slider = swiper.$items[swiper._current];
-        var $items = $slider.querySelectorAll('*[toggle-class]');
-        Array.prototype.forEach.call($items, function($item, index){
-            var clazz = $item.getAttribute('toggle-class').split(/\s+/);
-            for(var i = 0, len = clazz.length; i < len; i++){
-                (function(i){
-                    it('.' + clazz, function(done){
-                        setTimeout(function(){
-                            assert($item.className.indexOf(clazz[i]) !== -1);
-                            done();
-                        }, 1000);
-                    });
-                })(i);
-            }
-        });
-    });
+describe('.next()', function (){
+    it('should translateY the container to the slide height & should add classes to dom', function (){
+        assert(swiper.$container.style['-webkit-transform'] === '');
 
-    describe('.next()', function (){
-        it('should translateY the container to the slide height & should add classes to dom', function (){
-            assert(swiper.$container.style['-webkit-transform'] === '');
+        for(var i = 1, len = swiper.$items.length; i < len; i++){
+            swiper.next();
+            var height = swiper.$items[i].style['height'].replace('px', '');
+            var expect = 'translate3d(0px, -'+ (height * i) +'px, 0px)';
 
-            for(var i = 1, len = swiper.$items.length; i < len; i++){
-                swiper.next();
-                var height = swiper.$items[i].style['height'].replace('px', '');
-                var expect = 'translate3d(0px, -'+ (height * i) +'px, 0px)';
-
-                assert(swiper._current === i);
-                assert(swiper.$container.style['-webkit-transform'] === expect);
-            }
-        });
+            assert(swiper._current === i);
+            assert(swiper.$container.style['-webkit-transform'] === expect);
+        }
     });
 });
 
 
+describe('.on()', function (){
+    it('swiped listener should not be empty', function (){
 
+        swiper.on('swiped', function (){
+
+        });
+        assert(typeof swiper._eventHandlers['swiped'] === 'function');
+    })
+});
